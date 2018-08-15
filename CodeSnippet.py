fd56c0a771e79fd3faba357e9bda36484e157368
yekai1024 <yekai1024@gmail.com>
2018-08-15 11:47:44 +0800

    Update CodeSnippet.py

--- CodeSnippet.py
+++ CodeSnippet.py
@@ -10,3 +10,23 @@ def expected_failure(test):
 @expected_failure
 def test002_run_cmd_timeout(self):
     pass
+
+# https://stackoverflow.com/questions/2490334/simple-way-to-encode-a-string-according-to-a-password
+# Simple solution for encode/decode
+import base64
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
